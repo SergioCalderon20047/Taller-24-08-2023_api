a728aeaa2f1e55770f5da85fd3b3652365f12dff
Sergio Alejandro Calderon Perdomo <114233732+SergioCalderon20047@users.noreply.github.com>
2023-08-25 10:53:35 -0500

    API-actividad5_solo
    Actializacion de como lo mando la instructora

---
+++ App.py
@@ -0,0 +1,31 @@
+from flaskr import create_app
+from .modelos import Cancion, Usuario, db
+from .modelos import Album, db
+from .modelos import Medio
+from .modelos import AlbumSchema
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+with app.app_context():
+    u = Usuario(nombre='juan', contrasenia='12345')
+    c = Cancion(titulo="prueba", minutos=2, segundos=25, interprete="Sergio")
+    a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    u.albumes.append(a)
+    a.canciones.append(c)
+    db.session.add(u)
+    db.session.add(c)
+    db.session.add(A)
+    db.session.commit()
+    print([Album_Schema.dumps(album) for album in Album.query.all()])
+    print(Album.query.all())
+    print(Album.query.all()[0].cancion)
+    db.session.delete(a)
+    print(Album.query.all())
+    print(Cancion.query.all())
+
+
