04b45b2a1c67d1b4947bf8e3b359c8f434e489a1
Sergio Alejandro Calderon Perdomo <114233732+SergioCalderon20047@users.noreply.github.com>
2023-08-25 11:37:52 -0500

    Add files via upload

---
+++ vistas/vistas.py
@@ -0,0 +1,39 @@
+from flask _restful import Resource
+from .modelos import db, Cancion, CancionSchema
+from flask import request
+
+
+cancion_schema = CancionSchema()
+
+class VistaCanciones(Resource):
+    def get(self):#me trae todas las canciones
+        return [cancion_schema.dump(Cancion) for Cancion in Cancion.query.all()]
+
+    def post(self):
+        nueva_cancion = Cancion(titulo=request.json['titulo'], \
+                                minutos=request.json['minutos'], \
+                                segundos=request.json['segundos'], \
+                                interprete=request.json['interprete'], \)
+        db.session.add(nueva_cancion)#agregar a la db
+        db.session.commit()#guardar los cambios
+        return cancion_schema.dump(nueva_cancion)#retorna la nueva cancion en formato json
+
+
+class VistaCancion(Resource):
+
+    def get(self, id_cancion):
+        return cancion_schema.dump(Cancion.query.get_or_404(id_cancion))
+    def put(self, id_cancion):
+        cancion = Cancion.query.get_or_404(id_cancion)
+        cancion.titulo = request.json.get('titulo', cancion.titulo)
+        cancion.minutos = request.json.get('minutos', cancion.minutos)
+        cancion.segundos = request.json.get('segundos', cancion.segundos)
+        cancion.interprete= request.json.get('interprete', cancion.interprete)
+        db.session.commit()
+        return cancion_schema.dump(cancion)
+
+    def delete(self, id_cancion):
+        cancion = Cancion.query.get_or_404(id_cancion)
+        db.session.delete(cancion)
+        db.session.commit()
+        return 'operacion exitosa', 204
